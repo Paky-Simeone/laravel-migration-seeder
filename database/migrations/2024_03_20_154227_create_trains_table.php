d263a9ea5aef3c57a5026407fd6f5fe6e0679439
Paky-Simeone <pasquale.simeone.46@gmail.com>
2024-03-20 16:56:06 +0100

    add create table trains

---
+++ database/migrations/2024_03_20_154227_create_trains_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->char('Azienda', 20);
+            $table->string('Stazione_di_partenza', 30);
+            $table->string('Stazione_di_arrivo', 30);
+            $table->time('Orario_di_partenza');
+            $table->time('Orario_di_arrivo');
+            $table->char('Codice_Treno', 20)->unique('code');
+            $table->tinyInteger('Numero_Carrozze');
+            $table->boolean('In_Orario')->default(true);
+            $table->boolean('Cancellato')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trains');
+    }
+};
